9f6509b8bcfe5e884cae33b3ccf4f3e065779c76
dhvanil3103 <105095994+dhvanil3103@users.noreply.github.com>
2023-04-24 15:37:09 +0530

    Update Twitter__Sentimental_Analysis.py

--- Twitter__Sentimental_Analysis.py
+++ Twitter__Sentimental_Analysis.py
@@ -1,40 +1,73 @@
+import streamlit as st
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
 from textblob import TextBlob
-pd.set_option('display.max_colwidth', None)
+
+st.set_page_config(page_title="Twitter Sentiment Analysis App")
+
+# Set page title and layout
+st.title("Twitter Sentiment Analysis App")
+st.sidebar.header("User Input")
+st.sidebar.write("Enter the keyword(s) to search for on Twitter:")
 
 # take user input for the search query
-search_query = input("Enter the Twitter search query: ")
+search_query = st.sidebar.text_input(label="", value="", max_chars=None, key=None, type='default')
 
 # create the query string with lang:en filter
 query = search_query + " lang:en"
 
-tweets =[]
-limit = 1
-for tweet in sntwitter.TwitterSearchScraper(query).get_items():
-    if len(tweets) == limit:
-        break
+# create a function to perform sentiment analysis
+def analyze_sentiment():
+    tweets =[]
+    limit = 10 # limit the number of tweets returned
+    for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+        if len(tweets) == limit:
+            break
+        else:
+            tweets.append([tweet.date, tweet.user.username, tweet.content])
+    
+    df= pd.DataFrame(tweets, columns=['Date','User','Tweet'])
+
+    # perform sentimental analysis on the tweets
+    sentiments = []
+    for tweet in df['Tweet']:
+        blob = TextBlob(tweet)
+        sentiment = blob.sentiment.polarity
+        sentiments.append(sentiment)
+
+    # add the sentiments to the dataframe
+    df['Sentiment'] = sentiments
+
+    # group the tweets by sentiment and count the number of tweets in each group
+    sentiment_counts = df.groupby('Sentiment').size().reset_index(name='Count')
+
+    # calculate the percentage of tweets in each sentiment group
+    total_tweets = sentiment_counts['Count'].sum()
+    sentiment_counts['Percentage'] = sentiment_counts['Count'] / total_tweets * 100
+
+    # return the sentiment analysis results
+    return sentiment_counts
+
+# create a function to display the sentiment analysis results
+def display_results(results):
+    st.subheader("Sentiment Analysis Results:")
+    st.write(results)
+    # display a summary of the sentiment analysis results
+    if len(results) > 0:
+        sentiment = results['Sentiment'][0]
+        if sentiment > 0:
+            st.write("Overall sentiment: Positive")
+        elif sentiment < 0:
+            st.write("Overall sentiment: Negative")
+        else:
+            st.write("Overall sentiment: Neutral")
     else:
-        tweets.append([tweet.date, tweet.user.username, tweet.content])
+        st.write("No tweets found for the entered keyword(s).")
 
-df= pd.DataFrame(tweets, columns=['Date','User','Tweet'])
-print(df)
+# add a submit button to trigger sentiment analysis
+submit = st.sidebar.button("Submit")
 
-# perform sentimental analysis on the tweets
-sentiments = []
-for tweet in df['Tweet']:
-    blob = TextBlob(tweet)
-    sentiment = blob.sentiment.polarity
-    sentiments.append(sentiment)
-
-# add the sentiments to the dataframe
-df['Sentiment'] = sentiments
-
-# group the tweets by sentiment and count the number of tweets in each group
-sentiment_counts = df.groupby('Sentiment').size().reset_index(name='Count')
-
-# calculate the percentage of tweets in each sentiment group
-total_tweets = sentiment_counts['Count'].sum()
-sentiment_counts['Percentage'] = sentiment_counts['Count'] / total_tweets * 100
-
-print(sentiment_counts)
+# perform sentiment analysis and display the results when the submit button is clicked
+if submit:
+    results = analyze_sentiment()
+    display_results(results)
